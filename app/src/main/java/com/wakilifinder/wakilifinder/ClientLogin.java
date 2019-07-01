bab9b034bffa60903e6252ce5431331a8d52694f
MuhiaKevin <archerkreiger@gmail.com>
2019-07-01 11:25:27 +0300

    Initial commit Redone Everything from scratch again

---
+++ app/src/main/java/com/wakilifinder/wakilifinder/ClientLogin.java
@@ -0,0 +1,13 @@
+package com.wakilifinder.wakilifinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_login);
+    }
+}
