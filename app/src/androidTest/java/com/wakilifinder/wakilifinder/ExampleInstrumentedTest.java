28339eda52399b00e4e8627345864205b3328bd9
MuhiaKevin <archerkreiger@gmail.com>
2019-07-03 13:42:42 +0300

    Switched project to AndroidX and added firebase database and auth libraries

--- app/src/androidTest/java/com/wakilifinder/wakilifinder/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/wakilifinder/wakilifinder/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package com.wakilifinder.wakilifinder;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- app/src/main/java/com/wakilifinder/wakilifinder/ClientSignup.java
+++ app/src/main/java/com/wakilifinder/wakilifinder/ClientSignup.java
@@ -1,6 +1,6 @@
 package com.wakilifinder.wakilifinder;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ClientSignup extends AppCompatActivity {
