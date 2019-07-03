648234e36ccf40922f6f5d14bbab607fe403c8e3
MuhiaKevin <archerkreiger@gmail.com>
2019-07-03 12:11:41 +0300

    Added signup view and finished client part

--- app/src/main/java/com/wakilifinder/wakilifinder/ClientLogin.java
+++ app/src/main/java/com/wakilifinder/wakilifinder/ClientLogin.java
@@ -1,13 +1,30 @@
 package com.wakilifinder.wakilifinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ClientLogin extends AppCompatActivity {
+
+    private Button clientsignupbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_login);
+
+        clientsignupbtn =  findViewById(R.id.clientSignupbtn);
+
+        clientsignupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(),ClientSignup.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
