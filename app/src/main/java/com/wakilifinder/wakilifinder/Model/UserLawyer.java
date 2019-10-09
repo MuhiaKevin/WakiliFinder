d2aec6ccffacd96cd0988b787253b331aad9e41d
Kevin Muhia <archerkreiger@gmail.com>
2019-10-09 23:01:30 +0300

    added username field for Lawyers in database

--- app/src/main/java/com/wakilifinder/wakilifinder/Model/UserLawyer.java
+++ app/src/main/java/com/wakilifinder/wakilifinder/Model/UserLawyer.java
@@ -4,6 +4,7 @@ package com.wakilifinder.wakilifinder.Model;
 
 public class UserLawyer {
     public String userid;
+    public String username;
     public String imageurl;
     public String email;
     public String p105number;
@@ -12,10 +13,11 @@ public class UserLawyer {
     public String status;
 
 
-    public UserLawyer(String userid,String imageurl, String email,String password, String p105number, String practicenum, String status){
+    public UserLawyer(String userid,String imageurl, String email,String password, String p105number, String practicenum,  String status, String username){
         this.imageurl = imageurl;
         this.userid = userid;
         this.email = email;
+        this.username = username;
         this.password = password;
         this.p105number = p105number;
         this.status = status;
@@ -81,5 +83,11 @@ public class UserLawyer {
     // alt + insert
 
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
